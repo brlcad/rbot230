f3e6e505e3d7a251c2567f62a82b10df5c87437f
Christopher Sean Morrison <brlcad@gmail.com>
2022-05-17 17:56:41 -0400

    initial stub

---
+++ identify_movement/identify_movement.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+
+
+int main(int ac, char *av[]) {
+  return 0;
+}
